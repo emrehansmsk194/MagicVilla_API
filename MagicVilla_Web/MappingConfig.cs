88934414f4f9c53fac69cf66f520dfb493f0ffba
Bekir Emrehan SIMSEK <emrehan.simsek@tkteknoloji.com.tr>
2024-07-10 16:51:37 +0300

    FirstViewCreated

---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaUpdateDTO,VillaDTO>().ReverseMap();
+            CreateMap<VillaCreateDTO,VillaDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO,VillaNumberUpdateDTO>().ReverseMap();
+            CreateMap<VillaNumberUpdateDTO,VillaNumberDTO>().ReverseMap();
+           
+
+        }
+    }
+}
---
+++ MagicVilla_Web/Models/APIResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System;
+
+namespace MagicVilla_Web
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode {  get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }  
+    }
+}
